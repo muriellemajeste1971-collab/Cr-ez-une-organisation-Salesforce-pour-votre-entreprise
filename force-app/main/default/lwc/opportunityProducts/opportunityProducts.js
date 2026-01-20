29ff80cf1a567ceab006391382dad8b161395ec3
First Last <you@email.com>
2026-01-20 17:16:56 +0100

    Display default message when no OpportunityLineItem is present

--- force-app/main/default/lwc/opportunityProducts/opportunityProducts.js
+++ force-app/main/default/lwc/opportunityProducts/opportunityProducts.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getProducts from '@salesforce/apex/OpportunityProductsController.getProducts';
 
-export default class OpportunityProducts extends LightningElement {}
+export default class OpportunityProducts extends LightningElement {
+    @api recordId;          // Id de l'opportunité
+    products = [];          // Liste des produits
+    hasProducts = false;    // Indique si l'opportunité contient des produits
+
+    @wire(getProducts, { opportunityId: '$recordId' })
+    wiredProducts({ data, error }) {
+        if (data) {
+            this.products = data;
+            this.hasProducts = data.length > 0;
+        } else if (error) {
+            console.error('Erreur lors de la récupération des produits : ', error);
+            this.products = [];
+            this.hasProducts = false;
+        }
+    }
+}
